c85ce94022010f1f8a818300be0327afa3d889f8
AyerArnott <nickmunuveyeova@gmail.com>
2023-12-24 13:37:04 +0300

    semantics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -5,22 +5,24 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
-
-    let mut vec1 = fill_vec(vec0);
+    let vec2 = vec![22,44,66];  //method 1 used to make a new vec with vec0 elements 
+    //let mut vec1 = fill_vec(vec2) //since we cannot take original vec as arg for fill_vec.
+    let mut vec1 = fill_vec(&vec0);
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+//methodd 2 is used to make fill_vec take a reference instead of orginal vec 
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut cloned_vec = vec.clone(); //clone() used to make acopy and store changes in var cloned_vec
 
-    vec.push(88);
+    cloned_vec.push(88);
 
-    vec
+    cloned_vec      //return copied vec
 }
